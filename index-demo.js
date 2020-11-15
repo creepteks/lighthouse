fbcc8b49eac60dbad20ddc49a84f9498fd4658de
Neo Baghani <mahdad.baghani@outlook.com>
2020-11-15 12:04:12 +0000

    feat(code): add guidance texts

--- index-demo.js
+++ index-demo.js
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
     console.log(decodeJWT(requestToken))  //log request token to console
     const uri = message.paramsToQueryString(message.messageToURI(requestToken), { callback_type: 'post' })
     const qr = transports.ui.getImageDataURI(uri)
-    res.send(`<div><img src="${qr}"/></div>`)
+    res.send(`<h3>Scan the QRCode to login to service using your DID</h3><div><img src="${qr}"/></div>`)
   })
 })
 
@@ -59,27 +59,25 @@ app.post('/verifyLogin', (req, res) => {
     // this is, then, sent to the holder (u-port mobile) where it is saved 
     // for later use
     const jwt = req.body.access_token
-    credentials.authenticateDisclosureResponse(jwt).then(creds => {
-      // take this time to perform custom authorization steps... then,
-      // set up a push transport with the provided 
-      // push token and public encryption key (boxPub)
-      const push = transports.push.send(creds.pushToken, creds.boxPub)
-  
-      credentials.createVerification({
-        sub: creds.did,
-        exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
-        claim: { 'Identity': { 'Last Seen': `${new Date()}` } }
-        // Note, the above is a complex (nested) claim. 
-        // Also supported are simple claims:  claim: {'Key' : 'Value'}
-      }).then(attestation => {
-        console.log(`Encoded JWT sent to user: ${attestation}`)
-        console.log(`Decodeded JWT sent to user: ${JSON.stringify(decodeJWT(attestation))}`)
-        return push(attestation)  // *push* the notification to the user's uPort mobile app.
-      }).then(res => {
-        console.log(res)
-        console.log('Push notification sent and should be recieved any moment...')
-        console.log('Accept the push notification in the uPort mobile application')
-      })
+    // take this time to perform custom authorization steps... then,
+    // set up a push transport with the provided 
+    // push token and public encryption key (boxPub)
+    const push = transports.push.send(creds.pushToken, creds.boxPub)
+
+    credentials.createVerification({
+      sub: creds.did,
+      exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
+      claim: { 'Identity': { 'Last Seen': `${new Date()}` } }
+      // Note, the above is a complex (nested) claim. 
+      // Also supported are simple claims:  claim: {'Key' : 'Value'}
+    }).then(attestation => {
+      console.log(`Encoded JWT sent to user: ${attestation}`)
+      console.log(`Decodeded JWT sent to user: ${JSON.stringify(decodeJWT(attestation))}`)
+      return push(attestation)  // *push* the notification to the user's uPort mobile app.
+    }).then(res => {
+      console.log(res)
+      console.log('Push notification sent and should be recieved any moment...')
+      console.log('Accept the push notification in the uPort mobile application')
     })
   }).catch(err => {
     console.log(err)
@@ -97,7 +95,7 @@ app.get('/verifyId', (req, res) => {
     console.log(decodeJWT(requestToken))  //log request token to console
     const uri = message.paramsToQueryString(message.messageToURI(requestToken), { callback_type: 'post' })
     const qr = transports.ui.getImageDataURI(uri)
-    res.send(`<div><img src="${qr}"/></div>`)
+    res.send(`<h3>Scan the QRCode to prove to service that you own the VC you have received</h3><div><img src="${qr}"/></div>`)
   })
 })
 
