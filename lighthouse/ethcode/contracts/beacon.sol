9f59ede5be183831b06e7fd7bc685caf39c1263e
Mahdad Baghani <mahdad.baghani@outlook.com>
2021-01-15 00:44:57 +0330

    feat(contracts): add control gate for double voting

--- lighthouse/ethcode/contracts/beacon.sol
+++ lighthouse/ethcode/contracts/beacon.sol
@@ -15,6 +15,7 @@ contract beacon is Verifier {
 
     function registerVoter(uint[9] calldata input) public {
         bytes memory hdid = abi.encode(input);
+        // bytesToBytes32 only reads the first 32 bytes, so should not worry about the 9th element of input 
         bytes32 hdid32 = byteutils.bytesToBytes32(hdid, 0);
         beacons[hdid32] = ballot ({state: ballotState.raw, encrypted: ''});
     }
@@ -27,13 +28,26 @@ contract beacon is Verifier {
     string memory ecnryptedVote) 
     public returns (bool result) {
         result = false;
+        // verify the zkp
         require(verifyTx(a, b, c, input), 'invalid zk proof. Abort');
 
         bytes memory hashed  = abi.encode(input);
+        // bytesToBytes32 only reads the first 32 bytes, so should not worry about the 9th element of input 
         bytes32 h32 = byteutils.bytesToBytes32(hashed, 0);
 
+        // check for duplicate votes
+        require(beacons[h32].state != ballotState.burnt, 'Already voted. Abort');
+        // recording the vote
         beacons[h32].encrypted = ecnryptedVote;
+        beacons[h32].state = ballotState.burnt;
 
         result = true;
+    }
+
+    function getVote(uint[9] memory input) public view returns (string memory v) {
+        bytes memory hashed  = abi.encode(input);
+        // bytesToBytes32 only reads the first 32 bytes, so should not worry about the 9th element of input 
+        bytes32 h32 = byteutils.bytesToBytes32(hashed, 0);
+        v = beacons[h32].encrypted;
     }
 }
