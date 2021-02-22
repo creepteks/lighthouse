b6f27dfcb37a51ccdfbb8779413dff80a4b17960
Mahdad Baghani <mahdad.baghani@outlook.com>
2021-02-22 21:08:56 +0330

    feat(contracts): prevent double voter registration

--- lighthouse/ethcode/contracts/beacon.sol
+++ lighthouse/ethcode/contracts/beacon.sol
@@ -6,18 +6,21 @@ import "./utils/byteutils.sol";
 
 contract beacon is Verifier {
     // whether the vote is still in voters position or has been cast into the ballot box
-    enum ballotState {raw, burnt }
+    enum ballotState {raw, registered, burnt }
     struct ballot {
         ballotState state;
         string encrypted;
     }
     mapping(bytes32 => ballot) public beacons;
 
-    function registerVoter(uint[9] calldata input) public {
+    function registerVoter(uint[9] calldata input) public returns (bool successful) {
         bytes memory hdid = abi.encode(input);
         // bytesToBytes32 only reads the first 32 bytes, so should not worry about the 9th element of input 
         bytes32 hdid32 = byteutils.bytesToBytes32(hdid, 0);
-        beacons[hdid32] = ballot ({state: ballotState.raw, encrypted: ''});
+        // make sure the voter is not being registered for the second time
+        require(beacons[hdid32].state == ballotState.raw, 'Voter already registered. Abort');
+        beacons[hdid32] = ballot ({state: ballotState.registered, encrypted: ''});
+        successful = true;
     }
 
     function vote(
@@ -36,7 +39,7 @@ contract beacon is Verifier {
         bytes32 h32 = byteutils.bytesToBytes32(hashed, 0);
 
         // check for duplicate votes
-        require(beacons[h32].state != ballotState.burnt, 'Already voted. Abort');
+        require(beacons[h32].state == ballotState.registered, 'Ballot is not valid. Abort');
         // recording the vote
         beacons[h32].encrypted = ecnryptedVote;
         beacons[h32].state = ballotState.burnt;
